f1641bf2f8117edea739762810e3ecec382ac37e
Neco Coneco <151455107+RomanSh-shell@users.noreply.github.com>
2025-10-31 17:44:26 +0500

    Update README.js

--- README.js
+++ README.js
@@ -1,9 +1,142 @@
+const days = {
+  monday: {
+    name: 'Расписание на понедельник',
+    id: '1rnYBzXaWr9dI1u8haxakKDiz7KK0IzjJ',
+    api: '',
+    gid: '0'
+  },
+  tuesday: {
+    name: 'Расписание на вторник',
+    id: '17Oz-Ld6NHA57SEKCiuAWQNYYngJ3_7LB',
+    api: '',
+    gid: '0'
+  },
+  wednesday: {
+    name: 'Расписание на среду',
+    id: '1Xt-HxVTrKzp4_3NcaBGbTYxKp_PGLeN2',
+    api: '',
+    gid: '0'
+  },
+  thursday: {
+    name: 'Расписание на четверг',
+    id: '1oessjK-CwC-p_Kf0y1be61ZRwHq4jd4l',
+    api: '',
+    gid: '0'
+  },
+  friday: {
+    name: 'Расписание на пятницу',
+    id: '1_m0C3uWV3XvPLAko0KZSJWq5JMMHe8HH',
+    api: '',
+    gid: '0'
+  }
+};
+
+const classes = {
+  individual: {
+    name: 'Индивидуалы, дз',
+    id: '1WDnmYHNnhmO9MdCdcR2mKRR9hTqthR84mqV_1VsU38w',
+    api: '',
+    gid: '0'
+  },
+  class5_1: {
+    name: '5-1 класс, дз',
+    id: '1mywfGN2EP0z2FIlWnmfVOwdbZoP1_I_tsTU0lqd5UfI',
+    api: '',
+    gid: '0'
+  },
+  class5_2: {
+    name: '5-2 класс, дз',
+    id: '1dYiQ0SiJGvlDGjyjfBIgMl1vgAdloSLf_kjkPlKSKOQ',
+    api: '',
+    gid: '0'
+  },
+  class6_1: {
+    name: '6-1 класс, дз',
+    id: '1zov6d36gXBvqY5Z5p-iNppVFMidI-dDsOYwfLaOLG5w',
+    api: '',
+    gid: '0'
+  },
+  class6_2: {
+    name: '6-2 класс, дз',
+    id: '1C_CxD9fUXJ5OtgiLmRwwGvIQd7MxE7uziAdWehGnkpU',
+    api: '',
+    gid: '0'
+  },
+  class7_1: {
+    name: '7-1 класс, дз',
+    id: '1uufK-YSTwzuOsI1WD8Ay8kAgDI-owGQ6pofxTaxPU1s',
+    api: '',
+    gid: '0'
+  },
+  class7_2: {
+    name: '7-2 класс, дз',
+    id: '14Kug6YztYre6iKs-qH47Paf-dwcJse0JW-zSLekhSxY',
+    api: '',
+    gid: '0'
+  },
+  class8_1: {
+    name: '8-1 класс, дз',
+    id: '11LKk9u8OyVdphPMA8dIVNGkz1EQVEYovFc_ZChayWtU',
+    api: '',
+    gid: '0'
+  },
+  class8_2: {
+    name: '8-2 класс, дз',
+    id: '1wQ13kwq009UbhaVyTgVIx8XQY1PiuWQrr6KBOx4G52Q',
+    api: '',
+    gid: '0'
+  },
+  class8_3: {
+    name: '8-3 класс, дз',
+    id: '1_Etr2b0OURFaJZAbXgj1l3TDKk7X0469CiMysGKEvsc',
+    api: '',
+    gid: '0'
+  },
+  class9_1: {
+    name: '9-1 класс, дз',
+    id: '1FMNCOHLbIRZrL822scLvZPiAvHnQY4bKr0YRGhNJ_n0',
+    api: '',
+    gid: '0'
+  },
+  class9_2: {
+    name: '9-2 класс, дз',
+    id: '10mwDQQxPaDd8t00kwQn7e593SBeDB2YLHxjHbcsW-Mw',
+    api: '',
+    gid: '0'
+  },
+  class10_1: {
+    name: '10-1 класс, дз',
+    id: '1PeSWxzLIlDNhT2ujqQz0mKnCAdt0iSQyo_XQMMeN2-g',
+    api: '',
+    gid: '0'
+  },
+  class10_2: {
+    name: '10-2 класс, дз',
+    id: '1-yU65SGDMPCSI59P4eVRNkcMtaxDdToGqG2-7Ay89Iw',
+    api: '',
+    gid: '0'
+  },
+  class11_1: {
+    name: '11-1 класс, дз',
+    id: '1D7AjubNMiNcZuKU0aA7MOvxGHOD8RFKYH6WvtMGua0s',
+    api: '',
+    gid: '0'
+  },
+  class11_2: {
+    name: '11-2 класс, дз',
+    id: '19WzVvOWy41xkinaKqjlGUmZu6htaGDLPuAstW-eeqVM',
+    api: '',
+    gid: '0'
+  }
+};
+
+
 let FAIL = false
 async function getRange(sheetConfig, range) {
   // Пробуем API
   try {
     const response = await fetch(
-      `https://sheets.googleapis.com/v4/spreadsheets/${sheetConfig.sheetId}/values/${range}?key=${sheetConfig.apiKey}`
+      `https://sheets.googleapis.com/v4/spreadsheets/${sheetConfig.id}/values/${range}?key=${sheetConfig.api}`
     );
     
     if (!response.ok) throw new Error(`[${sheetConfig.name}]: API failed`);
@@ -15,7 +148,7 @@ async function getRange(sheetConfig, range) {
     console.warn(`! Ошибка получения данных таблицы [${sheetConfig}] — неверный API, переход к обходу CORS |Получение API: https://ai2.appinventor.mit.edu/reference/other/googlesheets-api-setup.html`);
 
     const proxy = 'https://corsproxy.io/?';
-    const csvUrl = `https://docs.google.com/spreadsheets/d/${sheetConfig.sheetId}/export?format=csv&gid=${sheetConfig.gid}`;
+    const csvUrl = `https://docs.google.com/spreadsheets/d/${sheetConfig.id}/export?format=csv&gid=${sheetConfig.gid}`;
     const response2 = await fetch(proxy + encodeURIComponent(csvUrl));
     
     if (!response2.ok) { 
