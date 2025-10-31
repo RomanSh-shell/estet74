1bb60a2ef7ff9084e9ee4869699d12f21640cc37
Neco Coneco <151455107+RomanSh-shell@users.noreply.github.com>
2025-10-31 17:04:04 +0500

    Create SHEETS.js

---
+++ SHEETS.js
@@ -0,0 +1,131 @@
+const days = {
+  monday: {
+    name: 'Расписание на понедельник',
+    sheetId: '1rnYBzXaWr9dI1u8haxakKDiz7KK0IzjJ',
+    apiKey: '',
+    gid: '0'
+  },
+  tuesday: {
+    name: 'Расписание на вторник',
+    sheetId: '17Oz-Ld6NHA57SEKCiuAWQNYYngJ3_7LB',
+    apiKey: '',
+    gid: '0'
+  },
+  wednesday: {
+    name: 'Расписание на среду',
+    sheetId: '1Xt-HxVTrKzp4_3NcaBGbTYxKp_PGLeN2',
+    apiKey: '',
+    gid: '0'
+  },
+  thursday: {
+    name: 'Расписание на четверг',
+    sheetId: '1oessjK-CwC-p_Kf0y1be61ZRwHq4jd4l',
+    apiKey: '',
+    gid: '0'
+  },
+  friday: {
+    name: 'Расписание на пятницу',
+    sheetId: '1_m0C3uWV3XvPLAko0KZSJWq5JMMHe8HH',
+    apiKey: '',
+    gid: '0'
+  }
+};
+
+const classes = {
+  individual: {
+    name: 'Индивидуалы, дз',
+    sheetId: '1WDnmYHNnhmO9MdCdcR2mKRR9hTqthR84mqV_1VsU38w',
+    apiKey: '',
+    gid: '0'
+  },
+  class5_1: {
+    name: '5-1 класс, дз',
+    sheetId: '1mywfGN2EP0z2FIlWnmfVOwdbZoP1_I_tsTU0lqd5UfI',
+    apiKey: '',
+    gid: '0'
+  },
+  class5_2: {
+    name: '5-2 класс, дз',
+    sheetId: '1dYiQ0SiJGvlDGjyjfBIgMl1vgAdloSLf_kjkPlKSKOQ',
+    apiKey: '',
+    gid: '0'
+  },
+  class6_1: {
+    name: '6-1 класс, дз',
+    sheetId: '1zov6d36gXBvqY5Z5p-iNppVFMidI-dDsOYwfLaOLG5w',
+    apiKey: '',
+    gid: '0'
+  },
+  class6_2: {
+    name: '6-2 класс, дз',
+    sheetId: '1C_CxD9fUXJ5OtgiLmRwwGvIQd7MxE7uziAdWehGnkpU',
+    apiKey: '',
+    gid: '0'
+  },
+  class7_1: {
+    name: '7-1 класс, дз',
+    sheetId: '1uufK-YSTwzuOsI1WD8Ay8kAgDI-owGQ6pofxTaxPU1s',
+    apiKey: '',
+    gid: '0'
+  },
+  class7_2: {
+    name: '7-2 класс, дз',
+    sheetId: '14Kug6YztYre6iKs-qH47Paf-dwcJse0JW-zSLekhSxY',
+    apiKey: '',
+    gid: '0'
+  },
+  class8_1: {
+    name: '8-1 класс, дз',
+    sheetId: '11LKk9u8OyVdphPMA8dIVNGkz1EQVEYovFc_ZChayWtU',
+    apiKey: '',
+    gid: '0'
+  },
+  class8_2: {
+    name: '8-2 класс, дз',
+    sheetId: '1wQ13kwq009UbhaVyTgVIx8XQY1PiuWQrr6KBOx4G52Q',
+    apiKey: '',
+    gid: '0'
+  },
+  class8_3: {
+    name: '8-3 класс, дз',
+    sheetId: '1_Etr2b0OURFaJZAbXgj1l3TDKk7X0469CiMysGKEvsc',
+    apiKey: '',
+    gid: '0'
+  },
+  class9_1: {
+    name: '9-1 класс, дз',
+    sheetId: '1FMNCOHLbIRZrL822scLvZPiAvHnQY4bKr0YRGhNJ_n0',
+    apiKey: '',
+    gid: '0'
+  },
+  class9_2: {
+    name: '9-2 класс, дз',
+    sheetId: '10mwDQQxPaDd8t00kwQn7e593SBeDB2YLHxjHbcsW-Mw',
+    apiKey: '',
+    gid: '0'
+  },
+  class10_1: {
+    name: '10-1 класс, дз',
+    sheetId: '1PeSWxzLIlDNhT2ujqQz0mKnCAdt0iSQyo_XQMMeN2-g',
+    apiKey: '',
+    gid: '0'
+  },
+  class10_2: {
+    name: '10-2 класс, дз',
+    sheetId: '1-yU65SGDMPCSI59P4eVRNkcMtaxDdToGqG2-7Ay89Iw',
+    apiKey: '',
+    gid: '0'
+  },
+  class11_1: {
+    name: '11-1 класс, дз',
+    sheetId: '1D7AjubNMiNcZuKU0aA7MOvxGHOD8RFKYH6WvtMGua0s',
+    apiKey: '',
+    gid: '0'
+  },
+  class11_2: {
+    name: '11-2 класс, дз',
+    sheetId: '19WzVvOWy41xkinaKqjlGUmZu6htaGDLPuAstW-eeqVM',
+    apiKey: '',
+    gid: '0'
+  }
+};
